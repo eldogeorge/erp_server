dcb140df937d22b8970400d2e91acd65a539b422
unknown <eldogeorgenettadiyil@gmail.com>
2023-11-14 11:33:48 +0530

    completed

--- dataBase/connections.js
+++ dataBase/connections.js
@@ -1,7 +1,7 @@
 // CMAS 5 till to 14 line, then goto index.js
 const mongoose= require('mongoose')
 // asym.connect portName.URLName
-mongoose.connect(process.env.baseurl,{
+mongoose.connect(process.env.baseUrl,{
 // parser issue, remove connect error
 useNewUrlParser:true,
 //topology issue,
